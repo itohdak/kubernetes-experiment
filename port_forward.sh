d5d51bc2a3a26f7a51c7070cf356aa2a02deb605
itohdak <itohdak@gmail.com>
2021-09-01 16:54:51 +0900

    update port_forward.sh

--- port_forward.sh
+++ port_forward.sh
@@ -1,3 +1,6 @@
-kubectl -n istio-system port-forward svc/prometheus 9090:9090 > /dev/null 2>&1 &
-kubectl -n istio-system port-forward svc/grafana 3000:3000 > /dev/null 2>&1 &
-kubectl port-forward svc/locust-master 8089:8089 > /dev/null 2>&1 &
+LOG=./log/port_forward.log
+ERROR_LOG=./log/port_forward_error.log
+kubectl -n istio-system port-forward svc/prometheus 9090:9090 > $LOG 2> $ERROR_LOG &
+kubectl -n istio-system port-forward svc/grafana 3000:3000 > $LOG 2> $ERROR_LOG &
+kubectl port-forward svc/locust-master 8089:8089 > $LOG 2> $ERROR_LOG &
+kubectl port-forward svc/locust-master-loadtestmanager-sample 8089:8089 > $LOG 2> $ERROR_LOG &
