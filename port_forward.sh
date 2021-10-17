f0ab3b992e4d689ba5e61fc6c110a29228b049c5
itohdak <itohdak@gmail.com>
2021-10-17 15:21:46 +0900

    add namespace for port_forward.sh

--- port_forward.sh
+++ port_forward.sh
@@ -2,5 +2,5 @@ LOG=./log/port_forward.log
 ERROR_LOG=./log/port_forward_error.log
 kubectl -n istio-system port-forward svc/prometheus 9090:9090 > $LOG 2> $ERROR_LOG &
 kubectl -n istio-system port-forward svc/grafana 3000:3000 > $LOG 2> $ERROR_LOG &
-kubectl port-forward svc/locust-master 8089:8089 > $LOG 2> $ERROR_LOG &
-kubectl port-forward svc/locust-master-loadtestmanager-sample 8089:8089 > $LOG 2> $ERROR_LOG &
+kubectl port-forward -n loadtest svc/locust-master 8089:8089 > $LOG 2> $ERROR_LOG &
+kubectl port-forward -n loadtest svc/locust-master-loadtestmanager-sample 8089:8089 > $LOG 2> $ERROR_LOG &
